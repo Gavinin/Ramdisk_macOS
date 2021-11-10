4bfb0c585c14fba8c32372d7efaea56c268a0e9c
Gavinin <gavinw_work@outlook.com>
2021-11-10 10:52:58 +0800

    1.Fix Bug 2.Add Default capacity as 1GB

--- Ramdisk.sh
+++ Ramdisk.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
 #mount
-if [ -z "$(echo $1 | sed 's#[0-9]##g')" ]
+if [ ! -z "$1" ] && [ -z "$(echo $1 | sed 's#[0-9]##g')" ] 
 then
 	if [ ! -d "/Volumes/RamDisk" ]; then
 		Num1=$1
-		setopt nonomatch
 		let Num1=Num1*2048
 		diskutil erasevolume HFS+ 'RamDisk' `hdiutil attach -nomount ram://$Num1`
 	else
 		echo "RAM DISK has exist"
 	fi
+
 	
 #umount	
-elif [ "$1" == "umount" ] 
-then
-	diskutil eject /Volumes/RamDisk
-else	
-	echo "No Capacty"
-fi
+elif [ "$1" == "umount" ]
+	then
+		diskutil eject /Volumes/RamDisk
+	if [ $? -ne 0 ]; then
+		echo "No Capacty"
+	fi
 
+#Default is 1GB
+elif [ -z "$1"]
+	then
+	if [ ! -d "/Volumes/RamDisk" ] 
+	then
+		let Num_Default=1024*2048
+		diskutil erasevolume HFS+ 'RamDisk' `hdiutil attach -nomount ram://$Num_Default`
+	else
+		echo "RAM DISK has exist"
+	fi
+fi
